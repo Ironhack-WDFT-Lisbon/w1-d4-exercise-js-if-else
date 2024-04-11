973dc7d3cb6f15638308778e2043d0478a1ae766
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-11 22:34:59 +0100

    update after class

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 console.log("Ready to go! 🚀");
+
+const language = prompt(
+  "In which language do you want to see the message? (Spanish, French, Italian, English)"
+);
+
+// ------------------------------
+// Iteration 1
+// ------------------------------
+// if (language === "Spanish") {
+//   console.log("Hola, mundo!");
+// } else if (language === "French") {
+//   console.log("Bonjour, le monde!");
+// } else if (language === "Italian") {
+//   console.log("Ciao, mondo!");
+// } else {
+//   console.log("Hello, world!");
+// }
+
+// ------------------------------
+// Iteration 2 - Lowercase
+// ------------------------------
+// const lowerCaseLanguage = language.toLowerCase();
+
+// if (lowerCaseLanguage === "spanish") {
+//   console.log("Hola, mundo!");
+// } else if (lowerCaseLanguage === "french") {
+//   console.log("Bonjour, le monde!");
+// } else if (lowerCaseLanguage === "italian") {
+//   console.log("Ciao, mondo!");
+// } else {
+//   console.log("Hello, world!");
+// }
+
+// ------------------------------
+// Iteration 3 - Solve prompt cancel click
+// ------------------------------
+// let lowerCaseLanguage = "";
+
+// if (language) {
+//   lowerCaseLanguage = language.toLowerCase();
+// }
+
+// if (lowerCaseLanguage === "spanish") {
+//   console.log("Hola, mundo!");
+// } else if (lowerCaseLanguage === "french") {
+//   console.log("Bonjour, le monde!");
+// } else if (lowerCaseLanguage === "italian") {
+//   console.log("Ciao, mondo!");
+// } else {
+//   console.log("Hello, world!");
+// }
