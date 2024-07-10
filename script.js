759036de354760cfcddf83e7fd73afa232df2cc3
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-07-10 13:07:14 +0100

    update exercise jul 24

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("Ready to go! 🚀");
 
+const language = prompt(
+  "In which language do you want to see the message? (Spanish, French, Italian, English)"
+);
+
 // ------------------------------
 // Iteration 1
 // ------------------------------
