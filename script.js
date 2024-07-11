a63ce1a7584f82136ad394fe83aef64473680519
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-07-11 09:27:06 +0100

    update script

--- script.js
+++ script.js
@@ -3,15 +3,3 @@ console.log("Ready to go! 🚀");
 const language = prompt(
   "In which language do you want to see the message? (Spanish, French, Italian, English)"
 );
-
-// ------------------------------
-// Iteration 1
-// ------------------------------
-
-// ------------------------------
-// Iteration 2 - Lowercase
-// ------------------------------
-
-// ------------------------------
-// Iteration 3 - Solve prompt cancel click
-// ------------------------------
